15dac8908c1e82ce17df2f190a353b47f6602dc5
Sasha <frozeefrozee12345@gmail.com>
2020-12-17 16:59:15 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Слайдеры
 $(document).ready(function () {
   $('.rooms__inner').slick({
     dots: true,
@@ -16,3 +17,33 @@ $(document).ready(function () {
     autoSpeed: 15000,
   });
 });
+
+// Бургер меню
+const toggleMenu = document.querySelector('.nav__toggle'),
+  navMenu = document.querySelector('.nav__menu');
+
+toggleMenu.addEventListener('click', (event) => {
+  navMenu.classList.toggle('active');
+});
+
+// плавное перемещение по якорям
+$(document).ready(function () {
+  $(".nav__menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
+});
+
+// Стрелка для возвращения наверх страницы
+(function ($) {
+  $(function () {
+
+    $('#up').click(function () {
+      $('body,html').animate({ scrollTop: 0 }, 500);
+      return false;
+    });
+
+  });
+})(jQuery);  
